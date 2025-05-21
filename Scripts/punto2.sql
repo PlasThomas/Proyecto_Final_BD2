c8dc52c229c6bd6f8a042a83ff5b39c1c2677974
PlasThomas <joseplascencia346@aragon.unam.mx>
2025-05-21 11:10:33 -0600

    Tabla concentrado ventas

--- Scripts/punto2.sql
+++ Scripts/punto2.sql
@@ -1,35 +1,5 @@
-use ecommerce;
-
-show tables;
-
-SELECT * FROM categoria;
-SELECT * FROM cliente;
-SELECT * FROM detalle_pedido;
-SELECT * FROM envio;
-SELECT * FROM estado;
-SELECT * FROM pais;
-SELECT * FROM pedido;
-SELECT * FROM producto;
-SELECT * FROM producto_promocion;
-SELECT * FROM promocion;
-SELECT * FROM subcategoria;
-SELECT * FROM tienda;
-
-desc categoria;
-desc cliente;
-desc detalle_pedido;
-desc envio;
-desc estado;
-desc pais;
-desc pedido;
-desc producto;
-desc producto_promocion;
-desc promocion;
-desc subcategoria;
-desc tienda;
-
+USE ecommerce;
 -- Trigger 1
-
 -- Creacion de tabla concentrado_ventas
 CREATE TABLE concentrado_ventas(
   id_concentrado_ventas INT AUTO_INCREMENT PRIMARY KEY,
@@ -38,7 +8,8 @@ CREATE TABLE concentrado_ventas(
   estatus_pedido VARCHAR(50) NOT NULL,
   metodo_pago VARCHAR(50) NOT NULL,
   avg_descuento DECIMAL(10,2) NOT NULL,
-  total_transacciones
+  total_transacciones INT NOT NULL,
+  FOREIGN KEY (id_tienda) REFERENCES tienda(tienda_id)
 );
 
 select from;
