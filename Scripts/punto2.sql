6c98ef4b7009598660c356f6633b9ded3c3789df
PlasThomas <joseplascencia346@aragon.unam.mx>
2025-05-21 23:37:01 -0600

    Trigger 2 y estructura de consulta de trigger 1

--- Scripts/punto2.sql
+++ Scripts/punto2.sql
@@ -1,9 +1,12 @@
 USE ecommerce;
--- Trigger 1
--- Creacion de tabla concentrado_ventas
+--<============================ Trigger 1 ============================> -- 
+
+-- #### Creacion de tabla concentrado_ventas
+DROP TABLE IF EXISTS concentrado_ventas;
 CREATE TABLE concentrado_ventas(
   id_concentrado_ventas INT AUTO_INCREMENT PRIMARY KEY,
   id_tienda INT NOT NULL,
+  fecha_transaccion DATE NOT NULL,
   total_venta DECIMAL(10,2) NOT NULL,
   estatus_pedido VARCHAR(50) NOT NULL,
   metodo_pago VARCHAR(50) NOT NULL,
@@ -12,8 +15,21 @@ CREATE TABLE concentrado_ventas(
   FOREIGN KEY (id_tienda) REFERENCES tienda(tienda_id)
 );
 
-select from;
+-- prueba de consulta
+SELECT t.tienda_id, p.fecha_pedido, 
+	SUM(dp.cantidad * dp.precio_unitario * (1-dp.descuento_aplicado/100)) as venta_total,
+  p.estatus , p.metodo_pago,
+  AVG(dp.descuento_aplicado) promedio_descuento,
+  COUNT(DISTINCT p.pedido_id) AS transaciones_totales
+FROM pedido as p
+	join detalle_pedido as dp on (p.pedido_id = dp.pedido_id)
+  join envio as e on (p.pedido_id = e.pedido_id)
+  join tienda as t on (e.tienda_id = t.tienda_id)
+  group by p.fecha_pedido, t.tienda_id
+  order by t.tienda_id;
 
+
+-- #### CREACION DEL TRIGGER 
 DELIMITER $$
 DROP PROCEDURE IF EXISTS total_ventas_diario$$
 CREATE PROCEDURE total_ventas_diario()
@@ -26,18 +42,28 @@ CALL total_ventas_diario();
 
 
 
--- Trigger 2
+--<============================ Trigger 2 ============================> -- 
+
+-- Se utilizo la exprecion "dp.cantidad * dp.precio_unitario * (1-dp.descuento_aplicado/100)" para el consumo para considerar toda la informacion disponible dentro de la misma base de datos y tener datos mas exactos
 
 DELIMITER $$
 DROP PROCEDURE IF EXISTS consumo_cliente_tiendas$$
 CREATE PROCEDURE consumo_cliente_tiendas(
-    IN ncliente VARCHAR(50)
+    IN ncliente INT
 )
 BEGIN
-
+	SELECT e.tienda_id,SUM(dp.cantidad * dp.precio_unitario * (1-dp.descuento_aplicado/100)) AS total_consumo, 
+  COUNT(DISTINCT p.pedido_id) AS numero_de_pedidos, 
+  AVG(dp.cantidad * dp.precio_unitario * (1-dp.descuento_aplicado/100)) AS promedio_consumo
+	FROM pedido AS p 
+	JOIN detalle_pedido AS dp ON(p.pedido_id = dp.detalle_id)
+	JOIN envio AS e ON(p.pedido_id = e.pedido_id)
+	WHERE p.cliente_id = ncliente
+	GROUP BY e.tienda_id;
 END$$
 DELIMITER ;
 
+-- se tiene que ingresar el id del cliente como argumento del procedimiento almacenado
 CALL consumo_cliente_tiendas();
 
 
